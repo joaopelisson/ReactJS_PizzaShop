fe75ae1bde7cab2c45064fdcda0972d1b2fffe6c
joaopelisson <joaopedro.devbr@gmail.com>
2024-01-03 10:09:47 -0300

    feat(ui): Page Login

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,35 @@
+import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 
 export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <div>SignIn</div>
+      <main className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="tracking-tighti text-2xl font-semibold">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail:</Label>
+              <Input type="email" id="email" />
+            </div>
+            <Button className="w-full" type="submit">
+              Acessar painel
+            </Button>
+          </form>
+        </div>
+      </main>
     </>
   )
 }
