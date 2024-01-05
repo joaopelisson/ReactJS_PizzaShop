6fe6b88f4f4e0fc79369ad6af4396e33beb253a5
joaopelisson <joaopedro.devbr@gmail.com>
2024-01-05 14:47:53 -0300

    feat: add toast (sonner)

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,6 +1,7 @@
 import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -20,8 +21,19 @@ export function SignIn() {
   } = useForm<SignInFormType>()
 
   async function handleSignIn(data: SignInFormType) {
-    console.log(data)
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    try {
+      console.log(data)
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success(`Enviamos um link de autentifcação para o seu e-mail!`, {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
+    } catch {
+      toast.error(`Ops.. parece que houve um problema com o e-mail informado!`)
+    }
   }
   return (
     <>
