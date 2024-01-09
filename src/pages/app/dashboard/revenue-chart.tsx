b4e7e2b6b254dd0ba9aeffd0d470cf456b29c11b
joaopelisson <joaopedro.devbr@gmail.com>
2024-01-09 09:19:14 -0300

    feat: revenue chart for the period

---
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,68 @@
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const data = [
+  { date: '09/01/2024', revenue: 1200 },
+  { date: '10/01/2024', revenue: 900 },
+  { date: '11/01/2024', revenue: 200 },
+  { date: '12/01/2024', revenue: 10 },
+  { date: '13/01/2024', revenue: 100 },
+  { date: '14/01/2024', revenue: 1150 },
+  { date: '15/01/2024', revenue: 750 },
+]
+
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no período
+          </CardTitle>
+          <CardDescription>Receita diária no período</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('pt-Br', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })
+              }
+            />
+            <XAxis dataKey="date" tickLine={false} axisLine={false} dy={17} />
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="revenue"
+              stroke={colors.violet[500]}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
