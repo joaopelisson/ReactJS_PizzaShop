f404f10694916fbcce45ee6660ff15b975b7e519
joaopelisson <joaopedro.devbr@gmail.com>
2024-01-31 06:47:51 -0300

    feat: loading state in profile

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,13 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true,
 })
+
+if (env.VITE_ENABLE_API_DELAY) {
+  const delay = 2000
+
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, delay))
+
+    return config
+  })
+}
